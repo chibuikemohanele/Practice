68aadc3e27c31f160d9636f00d8aa439607032c5
Chibuikem Ohanele <chibuiohanele@gmail.com>
2018-12-16 20:26:55 -0800

    Program that turns on red, green, and blue lights then stops after 5 cycles

--- led/src/main.c
+++ led/src/main.c
@@ -1,9 +1,60 @@
 #include <Arduino.h>
 
+int blue = 2;
+int green = 3;
+int red = 4;
+int built = 13;
+int count = 0;
+
 void setup() {
   // put your setup code here, to run once:
+  pinMode(built, OUTPUT);
+  pinMode(blue, OUTPUT);
+  pinMode(green, OUTPUT);
+  pinMode(red, OUTPUT);
 }
 
-void loop() {
+void setColor(int blueColor, int redColor, int greenColor)
+{
+  analogWrite(red, redColor);
+  analogWrite(blue, blueColor);
+  analogWrite(green, greenColor);
+}
+
+void loop()
+{
   // put your main code here, to run repeatedly:
+  if(count < 5)
+  {
+    //turns on red part of led
+    digitalWrite(built, HIGH);
+    digitalWrite(red, HIGH);
+    delay(1000);
+    digitalWrite(built, LOW);
+    digitalWrite(red, LOW);
+    delay(1000);
+
+    //turns on blue part of led
+    digitalWrite(built, HIGH);
+    digitalWrite(blue, HIGH);
+    delay(1000);
+    digitalWrite(built, LOW);
+    digitalWrite(blue, LOW);
+    delay(1000);
+
+    //turns on green part of led
+    digitalWrite(built, HIGH);
+    digitalWrite(green, HIGH);
+    delay(1000);
+    digitalWrite(built, LOW);
+    digitalWrite(green, LOW);
+    delay(1000);
+  }
+  else
+  {
+    exit(0);
+  }
+
+  count++;
+
 }
